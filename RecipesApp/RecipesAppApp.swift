5313c384ee1939b43965650f58967d0394112fb8
T-Mao <m737579372@gmail.com>
2025-05-27 18:33:24 -0700

    Initial Commit

---
+++ RecipesApp/RecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Tongze Mao on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
